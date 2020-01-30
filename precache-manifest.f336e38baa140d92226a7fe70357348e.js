9f7c8c33a9d41f61b8bcb8f4dcf84fde4412984f
sarmedrizvi <sarmedrizvi@gmail.com>
2020-01-30 13:48:58 +0500

    Updates

---
+++ precache-manifest.f336e38baa140d92226a7fe70357348e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9e415e2a27c1225cd7c01bf698cc32",
+    "url": "/FoodPredict/index.html"
+  },
+  {
+    "revision": "db14dc995af4a862f983",
+    "url": "/FoodPredict/static/css/main.b2d2bfa2.chunk.css"
+  },
+  {
+    "revision": "c128487a39fed44b873f",
+    "url": "/FoodPredict/static/js/2.523a3da4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FoodPredict/static/js/2.523a3da4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db14dc995af4a862f983",
+    "url": "/FoodPredict/static/js/main.078ea025.chunk.js"
+  },
+  {
+    "revision": "0f8d8fdf19948475b316",
+    "url": "/FoodPredict/static/js/runtime-main.9c293709.js"
+  }
+]);
